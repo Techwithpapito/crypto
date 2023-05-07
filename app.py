536cb88f0ddbcf805981ab1ea99a9ac3d5230e37
michael liendo <516932@student.saxion.nl>
2023-05-07 15:00:30 +0200

    fixed new errors

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/services')
+def services():
+    return render_template('diensten.html')
+
+@app.route('/about')
+def about():
+    return render_template('over_ons.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/why_crypto')
+def why_crypto():
+    return render_template('waarom_crypto.html')
+
+
+if __name__ == '__main__':
+    app.run()
